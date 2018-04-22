ccb49cf7e357a6a3a967b4ae9cbe0b696fe64a75
cristoffert <cristoffertorrealba@gmail.com>
2018-04-22 20:43:58 -0300

    termine

---
+++ CalculadoraSimple/Calculadora.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraSimple
+{
+    public class Calculadora
+    {
+        public int Sumar(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+        public int Sumar(string v1, string v2)
+        {
+            return Convert.ToInt16(v1) + Convert.ToInt16(v2);
+        }
+        public int SumarCero(string v1, string v2)
+        {
+            int valor1 = (v1.Equals("")) ? 0 : Convert.ToInt16(v1);
+            int valor2 = (v2.Equals("")) ? 0 : Convert.ToInt16(v2);
+
+            return valor1 + valor2;
+        }
+        public int Restar(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+        public int Multiplicar(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+        public int MultiplicarCero(int v1, int v2)
+        {
+            return v1==0 ? 0 : v2==0 ? 0 : v1*v2;
+            
+        }
+        public double Raiz(int v1)
+        {
+
+            return Math.Round(Math.Sqrt(v1), 2);
+
+        }
+        public double Potencia(double v1, double v2)
+        {
+
+            return (long)Math.Pow(v1, v2);
+
+        }
+        public double Division(int v1, int v2)
+        {
+
+            return v1/v2;
+
+        }
+        public decimal DivisionSinDecimal(int v1, int v2)
+        {
+            decimal valor;
+            valor= v1/v2;
+
+            return Math.Truncate(valor);
+
+        }
+        public int Residuo(int v1, int v2)
+        {
+
+            return v1 % v2;
+
+        }
+    }
+}
---
+++ ParcialSimpleTest/ParcialSimpleTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculadoraSimple;
+
+namespace ParcialSimpleTest
+{
+    [TestClass]
+    public class ParcialSimpleTest
+    {
+        [TestMethod]
+        public void SumarDosDigitosNumericos()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Sumar(2, 2);
+
+            Assert.AreEqual(4, resultado);
+        }
+        [TestMethod]
+        public void SumarDosDigitosCaracteres()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Sumar("2", "2");
+
+            Assert.AreEqual(4, resultado);
+        }
+        [TestMethod]
+        public void SumarDosDigitosCadenaVacia()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.SumarCero("", "");
+
+            Assert.AreEqual(0, resultado);
+        }
+        [TestMethod]
+        public void RestarDosDigitosNumericos()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Restar(3, 2);
+
+            Assert.AreEqual(1, resultado);
+        }
+        [TestMethod]
+        public void MultiplicarDosDigitosNumericos()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Multiplicar(3, 2);
+
+            Assert.AreEqual(6, resultado);
+        }
+        [TestMethod]
+        public void MultiplicarPorCero()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.MultiplicarCero(0, 2);
+
+            Assert.AreEqual(0, resultado);
+        }
+        [TestMethod]
+        public void CalcularRaizCuadrada()
+        {
+            Calculadora cal = new Calculadora();
+            double resultado = cal.Raiz(4);
+
+            Assert.AreEqual(2, resultado);
+        }
+        [TestMethod]
+        public void CalcularPotencia()
+        {
+            Calculadora cal = new Calculadora();
+            double resultado = cal.Potencia(2,2);
+
+            Assert.AreEqual(4, resultado);
+        }
+        [TestMethod]
+        public void CalcularDivision()
+        {
+            Calculadora cal = new Calculadora();
+            double resultado = cal.Division(4, 2);
+
+            Assert.AreEqual(2, resultado);
+        }
+        [TestMethod]
+        public void CalcularDivisionSinDecimal()
+        {
+            Calculadora cal = new Calculadora();
+            decimal resultado = cal.DivisionSinDecimal(3, 2);
+
+            Assert.AreEqual(1, resultado);
+        }
+        [TestMethod]
+        public void ResiduoDeDivision()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Residuo(3, 2);
+
+            Assert.AreEqual(1, resultado);
+        }
+    }
+}
